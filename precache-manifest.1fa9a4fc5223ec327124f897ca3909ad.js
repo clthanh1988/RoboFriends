eeef650e316b9e587d3f37d0dd5741a1e810933a
clthanh1988 <37326828+clthanh1988@users.noreply.github.com>
2019-07-15 08:39:58 +0700

    Updates

---
+++ precache-manifest.1fa9a4fc5223ec327124f897ca3909ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "786fea84c13e47013733a876a4205552",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "7235f4e878117c9a3d3b",
+    "url": "/RoboFriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "230bc1df4a6dc80fc8f6",
+    "url": "/RoboFriends/static/css/main.514aeeec.chunk.css"
+  },
+  {
+    "revision": "7235f4e878117c9a3d3b",
+    "url": "/RoboFriends/static/js/2.27ca1646.chunk.js"
+  },
+  {
+    "revision": "230bc1df4a6dc80fc8f6",
+    "url": "/RoboFriends/static/js/main.0b2f1783.chunk.js"
+  },
+  {
+    "revision": "d48d68f29921c5c588da",
+    "url": "/RoboFriends/static/js/runtime~main.19bee101.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
